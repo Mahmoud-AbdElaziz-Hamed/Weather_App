15569a476991d96569f513f5ce8d04a6edadd091
unknown <mahmoudabdelaziz93.mz@gmail.com>
2023-11-17 15:25:38 +0200

    create App

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+// ignore: unused_import
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/weather_provider.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+      create: (context) {
+        return WeatherProvider();
+      },
+      child: const WeatherApp()));
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Provider.of<WeatherProvider>(context).weatherData == null
+            ? Colors.blue
+            : Provider.of<WeatherProvider>(context)
+                .weatherData!
+                .getThemeColor(),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
